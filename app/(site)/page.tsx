6e699719de4a501c8ffc5243ca324c1f4d790c94
junc1508 <jun.chen08@hotmail.com>
2023-04-09 00:04:58 -0500

    fix apostrophe

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const projects = await getProjects();
   return (
   <div>
-    <h1 className="text-7xl font-extrabold">Hello I'm&nbsp;
+    <h1 className="text-7xl font-extrabold">Hello I&apos;m&nbsp;
     <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 
     bg-clip-text text-transparent">
        Jun
